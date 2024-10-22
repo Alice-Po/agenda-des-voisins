7ed57103f1aed2d2c0f0ce36e094937d951b06a5
Alice Poggioli <alice.poggioli@assemblee-virtuelle.org>
2024-10-22 14:44:28 +0200

    general config

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "YOUR NAME";
-export const SITE_DESCRIPTION = "Your Description Goes Here.";
+export const SITE_TITLE = "L'Agenda des Voisins";
+export const SITE_DESCRIPTION = "L'Agenda des Voisins — un mensuel papier distribué par un réseau de voisinage";
